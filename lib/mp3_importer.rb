5b1c9cde4d28efcacbfa49512cf802e8ee616f24
Malcolm Leake <echelon1089@gmail.com>
2019-03-27 03:45:52 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,3 +7,12 @@ def initialize(path)
 end
 
 
+def files
+Dir.entries(path).select {|entry| entry.include?(“.mp3”)}
+end
+
+def import
+ self.files.each {|filename| Song.new_by_filename(filename)}
+end
+
+end
